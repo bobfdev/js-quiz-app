1c16690f7d175f6cbca0c203d7e67fa0ea058b8a
Bob F <bobfdev@gmail.com>
2021-12-01 21:46:26 -0600

    updated files for deployment

--- game.js
+++ game.js
@@ -3,73 +3,90 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [];
 
-fetch('questions.json')
-    .then(res => {
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+    .then((res) => {
         return res.json();
     })
-    .then(loadedQuestions => {
-        questions = loadedQuestions;
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [ ...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0, 
+            loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            })
+
+            return formattedQuestion;
+        });
         startGame();
     })
-    .catch(err => {
+    .catch((err) => {
         console.error(err);
     });
 
-// Constants 
-
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [ ...questions];
+    availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        // go to end of page
+        //go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    // update progress bar
+    //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions(questionIndex);
+    currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
+    choices.forEach((choice) => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
-    })
+    });
 
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
         if (!acceptingAnswers) return;
-    
+
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
-        const classToApply = 
-            selectedAnswer === currentQuestion.answer ? 'correct' : 'incorrect';
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
         if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
@@ -81,11 +98,10 @@ choices.forEach(choice => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
-
     });
 });
 
-incrementScore = num => {
+incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
 };
